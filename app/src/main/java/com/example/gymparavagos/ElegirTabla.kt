684be6b3da15529023806b1e20bef25f19a5027b
LisardoCarretero <lisardokill@hotmail.com>
2022-10-27 17:51:06 +0200

    ahora mismo estoy haciendo un cuadro de dialo dialog_tablas.xml y me estoy pegando con la barra de arriba que no se cuadra bien una imagen :(

---
+++ app/src/main/java/com/example/gymparavagos/ElegirTabla.kt
@@ -0,0 +1,11 @@
+package com.example.gymparavagos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ElegirTabla : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.crear_tabla)
+    }
+}
