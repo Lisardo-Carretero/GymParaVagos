c4e5993c3865eb58ab8712ff6fc72a2e3f2a6cb6
LisardoCarretero <lisardokill@hotmail.com>
2022-10-30 16:49:21 +0100

    entendiendo como funciona el login con goole y modificacion visual del login

--- app/src/main/java/com/example/gymparavagos/LoginPage.kt
+++ app/src/main/java/com/example/gymparavagos/LoginPage.kt
@@ -1,30 +1,82 @@
 package com.example.gymparavagos
 
-import android.content.Intent
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.IntentSender
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.firebase.ktx.Firebase
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import java.util.zip.GZIPOutputStream
 
 
 class LoginPage : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.setContentView(R.layout.login)
         supportActionBar?.hide()
-        val botonLog: Button = findViewById(R.id.botonLogin)
-        botonLog.setOnClickListener { login() }
-    }
+        //Variables utiles
+        val botonGoogle: ImageButton = findViewById(R.id.bt_google)
+        val botonLog: ImageButton = findViewById(R.id.bt_login)
+        val userName: EditText = findViewById(R.id.textEdit_user)
+        val userPass: EditText = findViewById(R.id.textEdit_pass)
 
-    private fun login() {
-        val campoUsuario: String = findViewById<EditText>(R.id.userLogin).text.toString().trim().lowercase()
+        auth= FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+            .requestIdToken(getString(R.string.serverId)).requestEmail().build()
 
-        Toast.makeText(applicationContext, "Login correcto $campoUsuario", Toast.LENGTH_SHORT).show()
-        startActivity(Intent(this, UserPage::class.java).putExtra("nameUser", campoUsuario))
-        finish()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
 
+
+        oneTapClient = Identity.getSignInClient(this)
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(
+                BeginSignInRequest.PasswordRequestOptions.builder()
+                    .setSupported(true)
+                    .build()
+            )
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.serverId))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build()
+            )
+            // Automatically sign in when exactly one credential is retrieved.
+            .setAutoSelectEnabled(true)
+            .build()
+        oneTapClient.beginSignIn(signInRequest)
+            .addOnSuccessListener(this) { result ->
+                try {
+                    ActivityResultContracts.StartIntentSenderForResult()
+                } catch (e: IntentSender.SendIntentException) {
+                    Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                }
+            }
+            .addOnFailureListener(this) { e ->
+                // No saved credentials found. Launch the One Tap sign-up flow, or
+                // do nothing and continue presenting the signed-out UI.
+                e.localizedMessage?.let { Log.d(TAG, it) }
+            }
     }
 
 }
