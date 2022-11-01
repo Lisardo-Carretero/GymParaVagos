739526b797953a6cc029f4335fd7bb4d540f480c
LisardoCarretero <lisardokill@hotmail.com>
2022-11-01 16:14:55 +0100

    El login funciona por fin falta que si ya te has registrado con google puedas entrar xD y que te guarde las credenciales que es un coñazo iniciar sesion tol rato

--- app/src/main/java/com/example/gymparavagos/LoginPage.kt
+++ app/src/main/java/com/example/gymparavagos/LoginPage.kt
@@ -1,56 +1,75 @@
 package com.example.gymparavagos
 
+
 import android.app.Activity
-import android.content.ContentValues.TAG
-import android.content.Context
+import android.app.AlertDialog
 import android.content.Intent
-import android.content.IntentSender
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
-import com.google.android.gms.auth.api.signin.*
+import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
 import com.google.android.gms.tasks.Task
-import com.google.firebase.ktx.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
-import java.util.zip.GZIPOutputStream
 
 
 class LoginPage : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
-    private lateinit var googleSignInClient: GoogleSignInClient
-    private lateinit var oneTapClient: SignInClient
-    private lateinit var signInRequest: BeginSignInRequest
+    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        this.setContentView(R.layout.login)
-        supportActionBar?.hide()
-        //Variables utiles
-        val botonGoogle: ImageButton = findViewById(R.id.bt_google)
-        val botonLog: ImageButton = findViewById(R.id.bt_login)
-        val userName: EditText = findViewById(R.id.textEdit_user)
-        val userPass: EditText = findViewById(R.id.textEdit_pass)
+        setContentView(com.example.gymparavagos.R.layout.login)
 
-        auth = FirebaseAuth.getInstance()
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
-            .requestIdToken(getString(R.string.serverId)).requestEmail().build()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.serverId))
+            .requestEmail()
+            .build()
 
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
-        findViewById<ImageButton>(R.id.bt_google).setOnClickListener {
+        var googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        findViewById<ImageButton>(R.id.bt_login).setOnClickListener {
+            setup()
+        }
+
+        findViewById<SignInButton>(com.example.gymparavagos.R.id.bt_google).setOnClickListener {
             signInGoogle()
         }
     }
 
+    private fun setup() {
+        val userEditText: EditText = findViewById(R.id.textEdit_user)
+        val userEditPass: EditText = findViewById(R.id.textEdit_pass)
+        if (userEditText.text.isNotEmpty() && userEditPass.text.isNotEmpty()) {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                userEditText.text.toString(),
+                userEditPass.text.toString()
+            ).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    startActivity(
+                        Intent(this, UserPage::class.java).putExtra("email", userEditText.text.toString())
+                    )
+                    finish()
+                } else {
+                    val builder = AlertDialog.Builder(this)
+                    builder.setTitle("Error")
+                    builder.setMessage("Se ha producido un error")
+                    builder.setPositiveButton("Aceptar", null)
+                    builder.create()?.show()
+                }
+            }
+        }
+    }
+
+
+    /*lo que antes era oncreateactivity o algo asi. Ahora crea un launcher
+     que crea una vista para registrarte con google
+     */
     private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
@@ -59,31 +78,43 @@ class LoginPage : AppCompatActivity() {
     }
 
     private fun signInGoogle() {
-        val singInIntent = googleSignInClient.signInIntent
-        launcher.launch(singInIntent)
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+        )
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setAvailableProviders(providers)
+            .build()
+        launcher.launch(signInIntent)
     }
 
     private fun handleResults(task: Task<GoogleSignInAccount>) {
         if (task.isSuccessful) {
-            val account: GoogleSignInAccount = task.result
-            if (account != null)
+            val account: GoogleSignInAccount? = task.result
+            if (account != null) {
                 updateUI(account)
-        } else
-            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
-    }
-
-    private fun updateUI(account: GoogleSignInAccount) {
-        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-        auth.signInWithCredential(credential).addOnCompleteListener {
-            if (it.isSuccessful) {
-                val intent: Intent = Intent(this, UserPage::class.java)
-                intent.putExtra("naame", account.displayName)
-                startActivity(intent)
-            } else
-                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this, "joder TASK " + task.exception.toString(), Toast.LENGTH_SHORT).show()
         }
     }
 
+    private fun updateUI(account: GoogleSignInAccount) {
 
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Bienvenido " + account.email, Toast.LENGTH_SHORT).show()
+                val intent: Intent = Intent(this, UserPage::class.java)
+                intent.putExtra("name", account.displayName)
+                startActivity(intent)
+                finish()
+            } else {
+                startActivity(Intent(this, LoginPage::class.java))
+                finish()
+                Toast.makeText(this, "joder INTENT " + it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
-
--- app/src/main/java/com/example/gymparavagos/UserPage.kt
+++ app/src/main/java/com/example/gymparavagos/UserPage.kt
@@ -1,41 +1,40 @@
 package com.example.gymparavagos
 
-import android.R
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.text.Layout
-import android.util.Log
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.Toolbar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.get
-import com.google.android.material.appbar.AppBarLayout
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.ktx.Firebase
 
+enum class ProviderType {
+    EMAIL, GOOGLE
+}
 
 class UserPage : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.example.gymparavagos.R.layout.userpage)
-        auth = FirebaseAuth.getInstance()
-
-        val appbar: androidx.appcompat.widget.Toolbar = findViewById(com.example.gymparavagos.R.id.user_upBar)
-        appbar.title = intent.getStringExtra("name")
+        setContentView(R.layout.userpage)
+        updateUI()
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle item selection
+
         return when (item.itemId) {
-            com.example.gymparavagos.R.id.bt_logout -> {
-                auth.signOut()
-                startActivity(Intent(this, LoginPage::class.java))
+            R.id.bt_logout -> {
+                Toast.makeText(applicationContext, "AAAAAAAAAAAAAAAAAA", Toast.LENGTH_SHORT).show()
+                FirebaseAuth.getInstance().signOut()
+                onBackPressed()
                 return true
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun updateUI() {
+        this.actionBar?.title = intent.extras?.getString("email") ?: ""
     }
 }
